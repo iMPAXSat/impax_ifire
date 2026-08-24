1e3aa1de24a7a4a8743bd8c43c53e9139a5f98b6
Andrew Robbertz <24920994+Alrobbertz@users.noreply.github.com>
2026-08-24 11:33:51 -0400

    Force SWXSOC_MISSION Env Variable & Reconfigure on Package Import

--- impax_ifire/__init__.py
+++ impax_ifire/__init__.py
@@ -9,18 +9,17 @@ except ImportError:
     __version__ = "unknown version"
     version_tuple = (0, 0, "unknown version")
 
-# Get SWXSOC_MISSIONS environment variable if it exists or use default for mission
-SWXSOC_MISSION = os.getenv("SWXSOC_MISSION", "iMPAX")
-os.environ["SWXSOC_MISSION"] = SWXSOC_MISSION
-
-from swxsoc import config as swxsoc_config
-from swxsoc import log as swxsoc_log
+import swxsoc
 from swxsoc import print_config
 
-# Load user configuration
-config = swxsoc_config
+# Force the mission environment variable and reconfigure regardless of import order
+os.environ["SWXSOC_MISSION"] = "impax"
+swxsoc.reconfigure()
 
-log = swxsoc_log
+# Load user configuration
+config = swxsoc.config
+
+log = swxsoc.log
 
 # Then you can be explicit to control what ends up in the namespace,
 __all__ = ["config", "print_config"]
